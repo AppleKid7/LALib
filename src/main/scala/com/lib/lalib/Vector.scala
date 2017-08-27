9de7d281aa5dccfa2007bf18ab906d108ab815e8
AppleKid7 <kvltd00d@gmail.com>
2017-08-27 11:31:33 -0400

    refactored to greatly improve the addition method and the subtraction method, added a scalar multiplication method

--- src/main/scala/com/lib/lalib/Vector.scala
+++ src/main/scala/com/lib/lalib/Vector.scala
@@ -1,8 +1,11 @@
 package com.lib.lalib
 
 import scala.collection.immutable.List
+import scala.util.control.Breaks._
 
-class MVector(val coordinates: List[Double]) {
+case class MalformedVectorsException(message: String) extends Exception(message)
+
+class MVector(private var coordinates: List[Double]) {
   def ToString(): String = {
     var str = ""
     for(num <- coordinates){
@@ -15,4 +18,48 @@ class MVector(val coordinates: List[Double]) {
   def Equals(v: MVector): Boolean = {
     return coordinates == v.coordinates
   }
+
+  def Add(v: MVector): MVector = {
+    return MVector.AddHelper(this, v)
+  }
+
+  def Subtract(v: MVector): MVector = {
+    val negV = MVector.ScalarMultiply(-1.0, v)
+    return Add(negV)
+  }
+}
+
+object MVector {
+  def AddHelper(a: MVector, b: MVector): MVector = {
+    var bLength = b.coordinates.length
+    var aLength = a.coordinates.length
+    var newCoords = List[Double]()
+    var i = 0
+    while(i < aLength){
+      if(i < bLength) {
+        newCoords = newCoords :+ (a.coordinates(i) + b.coordinates(i))
+      } else {
+        newCoords = newCoords :+ a.coordinates(i)
+      }
+      i+=1
+    }
+    while(i < bLength){
+      if(i < aLength) {
+        newCoords = newCoords :+ (a.coordinates(i) + b.coordinates(i))
+      } else {
+        newCoords = newCoords :+ b.coordinates(i)
+      }
+      i+=1
+    }
+    return new MVector(newCoords)
+  }
+
+  def ScalarMultiply(s: Double, a: MVector): MVector = {
+    var newCoords = List[Double]()
+    for(i <- 0 to a.coordinates.length - 1) {
+      newCoords = newCoords :+ a.coordinates(i) * s
+    }
+    return new MVector(newCoords)
+  }
+
 }
--- src/test/scala/com/lib/lalib/VectorSpec.scala
+++ src/test/scala/com/lib/lalib/VectorSpec.scala
@@ -21,5 +21,41 @@ class VectorSpec extends Specification {
       val newVec = new MVector(List(1, 3, 4, 5))
       testVector.Equals(newVec) mustEqual false
     }
+    "addition should return the right sum of vectors for vectors of equal dimensions" in {
+      val newVec = new MVector(List(3, 1, 2))
+      val expected = new MVector(List(4, 3, 5))
+      val result = testVector.Add(newVec)
+      result.Equals(expected) mustEqual true
+    }
+    "addition should return the right sum of vectors when the first vector has fewer dimensions" in {
+      val newVec = new MVector(List(3, 1))
+      val expected = new MVector(List(4, 3, 3))
+      val result = testVector.Add(newVec)
+      result.Equals(expected) mustEqual true
+    }
+    "addition should return the right sum of vectors when the second vector has fewer dimensions" in {
+      val newVec = new MVector(List(3, 1, 2, 4))
+      val expected = new MVector(List(4, 3, 5, 4))
+      val result = testVector.Add(newVec)
+      result.Equals(expected) mustEqual true
+    }
+    "subtraction should return the right sum of vectors for vectors of equal dimensions" in {
+      val newVec = new MVector(List(3, 1, 2))
+      val expected = new MVector(List(-2, 1, 1))
+      val result = testVector.Subtract(newVec)
+      result.Equals(expected) mustEqual true
+    }
+    "subtraction should return the right sum of vectors when the first vector has fewer dimensions" in {
+      val newVec = new MVector(List(3, 1))
+      val expected = new MVector(List(-2, 1, 3))
+      val result = testVector.Subtract(newVec)
+      result.Equals(expected) mustEqual true
+    }
+    "subtraction should return the right sum of vectors when the second vector has fewer dimensions" in {
+      val newVec = new MVector(List(3, 1, 2, 4))
+      val expected = new MVector(List(-2, 1, 1, -4))
+      val result = testVector.Subtract(newVec)
+      result.Equals(expected) mustEqual true
+    }
   }
 }
